b21f9aaf1a9fc8ccff88f3011756cc2ac78cac2b
Emiko <emiko.yonekura@gmail.com>
2016-06-21 20:33:25 -0300

    Preg.2 ok

---
+++ Ejercicio02.rb
@@ -0,0 +1,10 @@
+a = [1,2,3]
+b = [:azul, :rojo, :amarillo]
+c = ["tacos", "quesadillas", "hamburguesas"]
+d = b.reverse
+
+for i in 0..2
+	
+	#puts a[i].to_s+" "+d[i].to_s+" "+c[i].to_s
+	puts "#{a[i]} #{d[i]} #{c[i]}"
+end
